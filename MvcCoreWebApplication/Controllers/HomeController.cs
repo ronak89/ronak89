478d67252a6ad2b02451e4e8a6d466b71e72bb90
Ronak Patel <patelronak1989@gmail.com>
2022-01-15 17:33:31 +0530

    change

--- MvcCoreWebApplication/Controllers/HomeController.cs
+++ MvcCoreWebApplication/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MvcCoreWebApplication.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+         
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
